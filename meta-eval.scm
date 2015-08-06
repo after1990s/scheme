a479c3119931efcaec8196560c68891ea652fbb1
after1990s <after1990s@gmail.com>
2015-08-05 23:50:50 -0700

    f

--- meta-eval.scm
+++ meta-eval.scm
@@ -1,6 +1,6 @@
 ;chapter 4.1 
 (define (eval exp env)
- (cond ((self-evaluation? exp))
+ (cond ((self-evaluating? exp))
      ((bool? exp) (eval-bool exp env));exercise 4.4 P259
      ((variable? exp) (lookup-variable-value exp env))
      ((quoted? exp) (text-of-quotation exp))
@@ -48,6 +48,12 @@
   (set-varialbe-value (assignment-variable exp)
                       (eval (assignment-value exp) env)
                       env))
+
+(define (eval-definition exp env)
+ (define-variable! (definition-variable exp)
+                   (eval (definition-value exp) env)
+                   env))
+
 (define (self-evaluating? exp)
   (cond ((number? exp) #t)
         ((string? exp) #t)
@@ -117,7 +123,7 @@
   (list 'if predicate consequent alternative))
 
 (define (begin? exp) 
-  (tagged-list exp 'begin))
+  (tagged-list? exp 'begin))
 
 (define (begin-action exp) (cdr exp))
 
@@ -173,4 +179,129 @@
        (else (eval-bool-and (cdr exp) env))))
 ;end of exercise 4.4
 
+(define (make-frame var val)
+ (cons var val))
 
+(define (frame-values frame) (cdr frame))
+
+(define (frame-variables frame) (car frame))
+
+(define (add-binding-to-frame! var val frame)
+ (set-car! frame (cons var (car frame)))
+ (set-cdr! frame (cons val (cdr frame))))
+
+(define (extend-environment vars vals base-env)
+ (if (= (length vars) (length vals))
+     (cons (make-frame vars vals) base-env)
+     (error "arguments and parameters length unaccepted")))
+
+(define (enclosing-enviroment env) (cdr env))
+
+(define (first-frame env) (car env))
+
+(define the-empty-environment '())
+
+(define (lookup-variable-value var env)
+ (define (env-loop env)
+  (define (scan vars vals)
+   (cond ((null? vars)
+          (env-loop (enclosing-environment env)))
+         ((eq? var (car vars)) (car vals))
+         (else (scan (cdr vars) (cdr vals)))))
+  (if (eq? env the-empty-environment)
+     (error "unbound variable" var)
+     (let ((frame (first-frame env)))
+      (scan (frame-variables frame) (frame-values frame)))))
+ (env-loop env))
+
+(define (set-variable-value! var val env)
+ (define (env-loop env)
+  (define (scan vars vals)
+   (cond ((null? vars) (env-loop (enclosing-environment env)))
+         ((eq? var (car vars)) 
+          (set-car! vals val))
+         (else (scan (cdr vars) (cdr vals)))))
+  (if (eq? env the-empty-environment)
+      (error "unbound variable" var)
+      (let ((frame (first-frame env)))
+       (scan (frame-varialbes frame) 
+             (frame-values frame)))))
+ (env-loop env))
+
+(define (define-variable! var val env)
+ (let ((frame (first-frame env)))
+  (define (scan vars vals)
+   (cond ((null? vars)
+          (add-binding-to-frame! var val frame))
+         ((eq? var (car vars))
+          (set-car! vals val))
+         (else (scan (cdr vars) (cdr vals)))))
+  (scan (frame-variables frame)
+        (frame-values frame))))
+;exercise 4.13 
+(define (undef-variable! var env)
+ (let ((frame (first-frame env)))
+  (define (scan vars)
+   (cond ((null? vars)
+          (error "symbol not found" var))
+         ((eq? var (car vars))
+          (make-unbound! var frame))
+         (else (scan (cdr vars) ))))
+  (scan (frame-variables frame)
+        )))
+;end of exercise 4.13
+
+(define (primitive-procedure? proc)
+ (tagged-list? proc 'primitive))
+
+(define (primitive-implementation proc) (cadr proc))
+
+(define primitive-procedures (list (list 'car car)
+                                   (list 'cdr cdr)
+                                   (list 'cons cons)
+                                   (list 'null? null?)
+                             ))
+(define (primitive-procedure-names)
+  (map car primitive-procedures))
+
+(define (primitive-procedure-objects)
+ (map (lambda (proc) (list 'primitive (cadr proc)))
+      primitive-procedures))
+
+(define (setup-environment)
+ (let ((initial-env
+        (extend-environment (primitive-procedure-names)
+                            (primitive-procedure-objects)
+                            the-empty-environment)))
+  (define-variable! 'true true initial-env)
+  (define-variable! 'false false initial-env)
+ initial-env))
+
+(define the-global-environment (setup-environment))
+
+(define input-prompt ";;; input:")
+(define output-prompt ";;; value:")
+
+
+(define (driver-loop)
+ (prompt-for-input input-prompt)
+ (let ((input (read)))
+  (let ((output (eval input the-global-environment)))
+   (announce-output output-prompt)
+   (user-print output)))
+ (driver-loop))
+
+(define (prompt-for-input str)
+ (newline) (newline) (display str) (newline))
+
+(define (announce-output str)
+ (newline) (newline) (display str) (newline))
+
+(define (user-print object)
+ (if (compound-procedure? object)
+     (display (list 'compound-procedure
+               (procedure-parameters object)
+               (procedure-body object)
+               '<procedure-env>))
+     (display object)))
+(driver-loop)
